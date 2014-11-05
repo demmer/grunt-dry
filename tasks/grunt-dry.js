be9cc730d20e8922c5a208b7bb69e19a1c46e2ef
Michael Demmer <demmer@jut.io>
2014-11-05 10:52:48 -0800

    initial version of build support using browserify

---
+++ tasks/grunt-dry.js
@@ -0,0 +1,59 @@
+var path = require('path');
+
+module.exports = function(grunt) {
+    grunt.option('stack', true);
+
+    var pkg = grunt.config.get('pkg');
+    if (! pkg) {
+        throw new Error('package.json must be loaded into grunt.config[pkg]');
+    }
+
+    // Can't use grunt.loadNpmTasks here because it uses the
+    // parent project's package.json file.
+    //
+    // Instead load all tasks from grunt-* packages in the
+    // devDependencies.
+    var dry_pkg = grunt.file.readJSON(path.join(__dirname, '../package.json'));
+    Object.keys(dry_pkg.devDependencies).forEach(function(pkg) {
+        if (/grunt-/.test(pkg)) {
+            grunt.task.loadTasks(path.join(__dirname, '../node_modules/', pkg, '/tasks'));
+        }
+    });
+
+    var main = pkg.main;
+    var browser = 'browser/' + pkg.name + '.js';
+    var sourcemap = browser + '.map';
+
+    grunt.config.set('browserify', {
+        bundle: {
+            options: {
+                browserifyOptions: {
+                    bundleExternal: false,
+                    standalone: pkg.name,
+                    debug: true
+                }
+            },
+            src: main,
+            dest: browser
+        }
+    });
+
+    grunt.config.set('exorcise', {
+        bundle: {
+            options: {},
+            files: [{
+                dest: sourcemap,
+                src: [browser]
+            }]
+        }
+    });
+
+    grunt.registerTask('build', [
+        'browserify',
+        'exorcise'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+};
